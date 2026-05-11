bac4c60be43c820c63d13b0ee534cf83b96af090
victoryst <mohannad.a.karim@gmail.com>
2026-05-11 18:55:36 +0300

    Remove unnecessary visualization

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -35,7 +35,6 @@ if __name__ == '__main__':
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    out = cv2.VideoWriter('data/videos/out_visualized.mp4', fourcc, fps, (width, height))
 
     license_plate = {}
     for car_id in np.unique(results['car_id']):
